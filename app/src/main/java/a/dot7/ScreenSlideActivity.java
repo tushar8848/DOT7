6802a99e9de1150b5593fa77f689b1239ac3a7f7
shadyns <sihmar.nitish@gmail.com>
2018-03-12 05:29:53 +0530

    Made new package for fragments and fixed imageView in sliding fragments by Nitish

--- app/src/main/java/a/dot7/ScreenSlideActivity.java
+++ app/src/main/java/a/dot7/ScreenSlideActivity.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import a.fragments.ScreenSlidePage_Fragment;
 import a.fragments.ScreenSlidePage_Fragment2;
@@ -20,6 +23,9 @@ public class ScreenSlideActivity extends FragmentActivity {
 
 private ViewPager mpager;
 private PagerAdapter mpageradapter;
+LinearLayout sliderDotsPanel;
+private int dotsCount;
+private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +34,49 @@ private PagerAdapter mpageradapter;
         mpager=findViewById(R.id.pager);
         mpageradapter=new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mpager.setAdapter(mpageradapter);
+        sliderDotsPanel = findViewById(R.id.SliderDots);
+        dotsCount = Min_Pages;
+        dots = new ImageView[dotsCount];
 
+        for(int i=0;i<dotsCount;i++){
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                    R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(0,8,0,8);
+            sliderDotsPanel.addView(dots[i],params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                R.drawable.active_dot));
+        mpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            int pos;
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                pos=position;
+                for(int i=0;i<dotsCount;i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                            R.drawable.nonactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+                        R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
-
     public void login(View view)
     {
         startActivity(new Intent(this,MainActivity.class));
     }
-
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter
     {
