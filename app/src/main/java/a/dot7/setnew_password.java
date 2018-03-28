68b34e8823f19445052d402e6ed765e4ec1cd54b
Pooja <poojasinghal16.ps@gmail.com>
2018-03-28 15:01:07 +0530

    Made otp enter pins

---
+++ app/src/main/java/a/dot7/setnew_password.java
@@ -0,0 +1,13 @@
+package a.dot7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class setnew_password extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setnew_password);
+    }
+}
