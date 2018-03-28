8e7c1983b07ab39c947e6e08d9ef220c36dab9f5
tushar8848 <tushargarg2897@outlook.com>
2018-03-28 05:57:37 +0530

    User registration and Auto OTP detection by Tushar

---
+++ app/src/main/java/a/common/SmsListener.java
@@ -0,0 +1,11 @@
+package a.common;
+
+import android.content.Intent;
+
+/**
+ * Created by TUSHAR on 28-03-18.
+ */
+
+public interface SmsListener {
+    public void messageReceived(String message);
+}
