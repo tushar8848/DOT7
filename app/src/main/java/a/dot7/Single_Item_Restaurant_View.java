ff33845940a14fd77509a975d93021c355d86239
Pooja <poojasinghal16.ps@gmail.com>
2018-03-31 11:32:44 +0530

    Made restaurant recycler view Page and single item restaurant view page.

---
+++ app/src/main/java/a/dot7/Single_Item_Restaurant_View.java
@@ -0,0 +1,18 @@
+package a.dot7;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Single_Item_Restaurant_View extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void add_to_fav(View view)
+    {
+
+    }
+}
