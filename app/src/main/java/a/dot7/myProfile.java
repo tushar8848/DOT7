854601e7ce22950db0c13605323bb23f910eb86b
swastikarai <swastikarai.mnnit@gmail.com>
2018-04-08 15:30:11 +0530

    Your Account Activity Added by Swastika

---
+++ app/src/main/java/a/dot7/myProfile.java
@@ -0,0 +1,13 @@
+package a.dot7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
