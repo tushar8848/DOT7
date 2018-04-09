5377ce216f2fda3f92492fac1279e3fc19b1be22
shadyns <sihmar.nitish@gmail.com>
2018-04-10 01:23:22 +0530

    Minor UI updates by ShadyNS

---
+++ app/src/main/java/a/dot7/About_Us.java
@@ -0,0 +1,21 @@
+package a.dot7;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class About_Us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about__us);
+
+        //Toolbar toolbar = findViewById(R.id.about_us_toolbar);
+        //setSupportActionBar(toolbar);
+
+       // if (getSupportActionBar() != null){
+         //   getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //}
+    }
+}
