ca8db4e9a4cd5166b0c0e48025416b04cc3f5c60
harneet singh <harneet.singh.ts@gmail.com>
2018-04-10 20:52:39 +0530

    User Address added by Harneet

--- app/src/main/java/a/Lifecycle_Restaurant_Ordering/Address_Recycler_View.java
+++ app/src/main/java/a/Lifecycle_Restaurant_Ordering/Address_Recycler_View.java
@@ -15,31 +15,64 @@ import a.dot7.R;
 
 public class Address_Recycler_View extends AppCompatActivity implements View.OnClickListener  {
 
-    private EditText FlatNo, StreetName, LandMark, State, District, Pincode;
+    private EditText FlatNo, StreetName, LandMark;
+    String Flatno, Street, Landmark;
     private AppCompatButton PlaceOrder;
+    private int flat = 0, sname=0, Land = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.place_order);
         setDetails();
+        Validate();
 
     }
 
     public void setDetails()
     {
         FlatNo = findViewById(R.id.Flat_Num);
-        StreetName = findViewById(R.id.StreetName);
-        LandMark = findViewById(R.id.Address);
-        State = findViewById(R.id.state);
-        District = findViewById(R.id.City);
-        Pincode = findViewById(R.id.Pincode);
+        StreetName = findViewById(R.id.street);
+        LandMark = findViewById(R.id.landmark);
         PlaceOrder = findViewById(R.id.appCompatButton2);
         PlaceOrder.setOnClickListener(this);
+        Flatno = FlatNo.getText().toString();
+        Street = StreetName.getText().toString();
+        Landmark = LandMark.getText().toString();
+    }
+
+    public void Validate()
+    {
+           if(Flatno.length()<0)
+           {
+               FlatNo.setError("This field is required");
+           }
+           else
+               flat = 1;
+
+           if(Street.length()<0)
+           {
+                StreetName.setError("This field is required");
+           }
+           else
+               sname = 1;
+
+           if(Landmark.length()<0)
+           {
+               LandMark.setError("This field is required");
+           }
+           else
+               Land = 1;
+
     }
 
     @Override
     public void onClick(View v) {
+
+        if(flat == 1 && sname == 1 && Land ==1)
+        {
+            
+        }
 
     }
 }
